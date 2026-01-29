409718f31c96c32d64c916ca8750038f06075ee4
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-01-29 22:55:52 +0700

    Update penilaian.py

--- admin/penilaian.py
+++ admin/penilaian.py
@@ -2,70 +2,65 @@ import streamlit as st
 from utils import fetch_one
 
 st.title("📝 Pemberian Skor Karyawan")
+if "penilaian" not in st.session_state:
+    st.session_state.penilaian = True
+    st.session_state.karyawan_id = None
 
 nama_karyawan = st.text_input("Nama Karyawan")
 departemen_karyawan = st.selectbox("Departemen Karyawan", ["Umum", "Teknis", "Pengolahan"])
-periode = st.selectbox("Periode Penilaian", ["jan-feb", "feb-mar", "mar-apr", 
+col1, col2 = st.columns(2)
+col1.selectbox("Periode Penilaian", ["jan-feb", "feb-mar", "mar-apr", 
                                              "apr-mei", "mei-jun", "jun-jul", 
                                              "jul-agu", "agu-sep", "sep-okt", 
                                              "okt-nov", "nov-des", "des-jan"])
+col2.selectbox("Tahun Penilaian", ["2022", "2023", "2024", 
+                                   "2025", "2026", "2027",
+                                   "2028", "2029", "2030"])
 
-if st.button("🔍 Cari Karyawan"):
+if st.button("🔍 Cari Karyawan", key="cari_karyawan"):
     karyawan = fetch_one(
         "SELECT * FROM user WHERE name_user = %s AND department = %s",
         (nama_karyawan.title(), departemen_karyawan)
     )
     if not karyawan:
         st.error("Karyawan tidak ditemukan. Silakan periksa kembali nama dan departemen.")
+        st.session_state.karyawan_id = None
     else:
         st.success(f"Karyawan {nama_karyawan.title()} ditemukan!")
         st.session_state.karyawan_id = karyawan['id_user']
-        st.session_state.nilai_akhir = None
 
-    st.markdown("---")
-    st.subheader("⭐ Skala Penilaian")
-    st.info("⭐ = Sangat Kurang\n\n⭐⭐ = Kurang\n\n⭐⭐⭐ = Cukup\n\n⭐⭐⭐⭐ = Baik\n\n⭐⭐⭐⭐⭐ = Sangat Baik")
+if st.session_state.karyawan_id is not None:
     # Form penilaian
-    with st.form("form_penilaian"):
-        col1, col2 = st.columns(2)
+    with st.form("form_penilaian", clear_on_submit=False):
+        with st.container(border=True):
+            st.subheader("Disiplin")
+            col_disiplin1, col_disiplin2 = st.columns(2)
+            ketepatan = col_disiplin1.number_input(f"**Ketepatan**", min_value=0, max_value=100, step=1, key="ketepatan")
+            absensi = col_disiplin2.number_input(f"**Absensi**", min_value=0, max_value=100, step=1, key="absensi")
+
+        with st.container(border=True):
+            st.subheader("Sikap Kerja")
+            col_sikap1, col_sikap2 = st.columns(2)
+            motivasi = col_sikap1.number_input(f"**Motivasi**", min_value=0, max_value=100, step=1, key="motivasi")
+            komunikasi = col_sikap2.number_input(f"**Komunikasi**", min_value=0, max_value=100, step=1, key="komunikasi")
+
+        with st.container(border=True):
+            st.subheader("Potensi Kemampuan")
+            col_potensi1, col_potensi2, col_potensi3 = st.columns(3)
+            pemahaman = col_potensi1.number_input(f"**Pemahaman dan Penugasan**", min_value=0, max_value=100, step=1, key="pemahaman")
+            pengembangan = col_potensi2.number_input(f"**Pengembangan Diri**", min_value=0, max_value=100, step=1, key="pengembangan")
+            teoritis = col_potensi3.number_input(f"**Teoritis**", min_value=0, max_value=100, step=1, key="teoritis")
+
+        with st.container(border=True):
+            st.subheader("Hasil Kerja")
+            col_kerja1, col_kerja2 = st.columns(2)
+            mutu = col_kerja1.number_input(f"**Mutu**", min_value=0, max_value=100, step=1, key="mutu")
+            kuantitas = col_kerja2.number_input(f"**Kuantitas**", min_value=0, max_value=100, step=1, key="kuantitas")
         
-        with col1:
-            st.write("**1. Kehadiran**")
-            kehadiran = st.feedback("stars", key="kehadiran")
-            
-            st.write("**2. Kualitas Kerja**")
-            kualitas_kerja = st.feedback("stars", key="kualitas_kerja")
-            
-            st.write("**3. Kedisiplinan**")
-            kedisiplinan = st.feedback("stars", key="kedisiplinan")
-
-            st.write("**4. Tanggung Jawab**")
-            tanggung_jawab = st.feedback("stars", key="tanggung_jawab")
-        
-        with col2:
-            st.write("**5. Kerjasama**")
-            kerjasama = st.feedback("stars", key="kerjasama")
-            
-            st.write("**6. Inisiatif**")
-            inisiatif = st.feedback("stars", key="inisiatif")
-
-            st.write("**7. Komunikasi**")
-            komunikasi = st.feedback("stars", key="komunikasi")
-
-            st.write("**8. Kreativitas**")
-            kreativitas = st.feedback("stars", key="kreativitas")
-
-        if st.form_submit_button("✅ hitung", use_container_width=True):
-            try:
-                total = (kehadiran + kualitas_kerja + kedisiplinan + 
-                        tanggung_jawab + kerjasama + inisiatif + 
-                        komunikasi + kreativitas) + 8
-                st.metric(label="Total Skor", value=(total-8)/(32)*100)
-                st.session_state.nilai_akhir = (total-8)/(32)*100
-            except Exception as e:
-                st.error("berikan penilaian terlebih dahulu")
-
-    if st.session_state.nilai_akhir is not None:
-
-        if st.button("💾 Simpan Penilaian"):
-            st.success("Penilaian berhasil disimpan!")
+        if st.form_submit_button("🔢 hitung", use_container_width=True, type="primary"):
+            total = (ketepatan*0.15 + absensi*0.15 + 
+                     motivasi*0.2 + komunikasi*0.2 +
+                     pemahaman*0.15 + pengembangan*0.10 + teoritis*0.10 + 
+                     mutu*0.15 + kuantitas*0.15)
+            st.metric(label="Total Skor", value=f"{(total/135)*100:.2f}")
+    
