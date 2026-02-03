cedf301093eb5b764f9680654f9a3e1b761c094c
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-02-03 23:45:56 +0700

    Update penilaian.py

--- admin/penilaian.py
+++ admin/penilaian.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from utils import fetch_one
 
-user = None
+if "user" not in st.session_state:
+    st.session_state.user = None
 
 st.title("📝 Pemberian Skor Karyawan")
 nama_karyawan = fetch_one("SELECT GROUP_CONCAT(name_user ORDER BY name_user) AS names FROM user")
@@ -18,7 +19,7 @@ with st.form("Pencarian Karyawan", border=False):
         tahun = st.selectbox("Tahun Penilaian", ["2022", "2023", "2024", "2025", 
                                                 "2026", "2027", "2028", "2029"])
     if st.form_submit_button("🔍 Cari", type="primary"):
-        user = fetch_one(""" 
+        st.session_state.user = fetch_one(""" 
                         SELECT
                             id_user,
                             department,
@@ -26,43 +27,46 @@ with st.form("Pencarian Karyawan", border=False):
                         FROM user
                         WHERE name_user = %s AND department = %s
                             """, (nama, unit))
-        if user:
-            st.toast(f"berhasil {user['name_user']}")
+        if st.session_state.user:
+            st.toast(f"berhasil {st.session_state.user['name_user']}")
         else :
             st.error("Karyawan tidak ditemukan, perbaiki nama atau departmentnya")
 
-if user:
+if st.session_state.user:
     # Form penilaian
     with st.form("form_penilaian", clear_on_submit=False):
         with st.container(border=True):
             st.subheader("Disiplin")
-            col_disiplin1, col_disiplin2 = st.columns(2)
-            ketepatan = col_disiplin1.number_input(f"**Ketepatan**", min_value=0, max_value=100, step=1, key="ketepatan")
-            absensi = col_disiplin2.number_input(f"**Absensi**", min_value=0, max_value=100, step=1, key="absensi")
+            col_disiplin1, col_disiplin2, col_disiplin3= st.columns(3)
+            ketepatan = col_disiplin1.number_input(f"**Ketepatan (10%)**", min_value=0, max_value=100, step=1, key="ketepatan")
+            kerapihan = col_disiplin2.number_input(f"**Kerapihan (10%)**", min_value=0, max_value=100, step=1, key="kerapihan")
+            waktu = col_disiplin3.number_input(f"**Waktu (10%)**", min_value=0, max_value=100, step=1, key="waktu")
 
         with st.container(border=True):
             st.subheader("Sikap Kerja")
             col_sikap1, col_sikap2 = st.columns(2)
-            motivasi = col_sikap1.number_input(f"**Motivasi**", min_value=0, max_value=100, step=1, key="motivasi")
-            komunikasi = col_sikap2.number_input(f"**Komunikasi**", min_value=0, max_value=100, step=1, key="komunikasi")
-
-        with st.container(border=True):
-            st.subheader("Potensi Kemampuan")
-            col_potensi1, col_potensi2, col_potensi3 = st.columns(3)
-            pemahaman = col_potensi1.number_input(f"**Pemahaman dan Penugasan**", min_value=0, max_value=100, step=1, key="pemahaman")
-            pengembangan = col_potensi2.number_input(f"**Pengembangan Diri**", min_value=0, max_value=100, step=1, key="pengembangan")
-            teoritis = col_potensi3.number_input(f"**Teoritis**", min_value=0, max_value=100, step=1, key="teoritis")
+            motivasi = col_sikap1.number_input(f"**Motivasi (15%)**", min_value=0, max_value=100, step=1, key="motivasi")
+            komunikasi = col_sikap2.number_input(f"**Komunikasi (15%)**", min_value=0, max_value=100, step=1, key="komunikasi")
 
         with st.container(border=True):
             st.subheader("Hasil Kerja")
-            col_kerja1, col_kerja2 = st.columns(2)
-            mutu = col_kerja1.number_input(f"**Mutu**", min_value=0, max_value=100, step=1, key="mutu")
-            kuantitas = col_kerja2.number_input(f"**Kuantitas**", min_value=0, max_value=100, step=1, key="kuantitas")
+            col_kerja1, col_kerja2, col_kerja3 = st.columns(3)
+            kualitas = col_kerja1.number_input(f"**Kualitas (15%)**", min_value=0, max_value=100, step=1, key="kualitas")
+            kuantitas = col_kerja2.number_input(f"**Kuantitas (10%)**", min_value=0, max_value=100, step=1, key="kuantitas")
+            tanggungjawab = col_kerja3.number_input(f"**Tanggungjawab (15%)**", min_value=0, max_value=100, step=1, key="tanggungjawab")
         
         if st.form_submit_button("✅ hitung", width='stretch', type="primary"):
-            total = (ketepatan*0.15 + absensi*0.15 + 
-                     motivasi*0.2 + komunikasi*0.2 +
-                     pemahaman*0.15 + pengembangan*0.10 + teoritis*0.10 + 
-                     mutu*0.15 + kuantitas*0.15)
-            st.metric(label="Total Skor", value=f"{(total/135)*100:.2f}")
+            total = (ketepatan*0.1 + kerapihan*0.1 + waktu*0.1 +
+                     motivasi*0.15 + komunikasi*0.15 +
+                     kualitas*0.15 + kuantitas*0.1 + tanggungjawab*0.15)
+            
+            hasil0, hasil1, hasil2, hasil3 = st.columns(4)
+            with hasil0:
+                st.metric("Total Skor", f"{total:.2f}")
+            with hasil1:
+                st.metric("Disiplin", f"{ketepatan*0.1 + kerapihan*0.1 + waktu*0.1:.2f}")
+            with hasil2:
+                st.metric("Sikap Kerja", f"{motivasi*0.15 + komunikasi*0.15:.2f}")
+            with hasil3:
+                st.metric("Hasil Kerja", f"{kualitas*0.15 + kuantitas*0.1 + tanggungjawab*0.15:.2f}")
     
