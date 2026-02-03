cac08b6428b09b24c7bb82403853ff1af0452910
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-02-03 23:44:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def login_page():
             colText.markdown("<p style='font-size: 20px; font-weight: 600;'>BPS Kabupaten Simalungun</p>", unsafe_allow_html=True)
             
             with st.form(key="login_form", border=True):
-                email = st.text_input("",placeholder="username")
-                password = st.text_input("",placeholder="Password", type="password")
+                email = st.text_input("e",placeholder="username", label_visibility='hidden')
+                password = st.text_input("p",placeholder="Password", type="password", label_visibility='collapsed')
 
                 if st.form_submit_button("Login", type="primary"):
                     with st.spinner("Memproses login..."):
@@ -64,24 +64,28 @@ def login_page():
 def logout(): 
     col1, col2 = st.columns([1.8,9])
     with col1:
-        if st.button("Batal"):
+        if st.button("Batal", key="reject"):
             st.stop()
 
     with col2:
-        if st.button("Ya, Keluar", type="primary"):
+        if st.button("Ya, Keluar", type="primary", key='accept'):
             with st.spinner("Memproses Logout.."):
                 st.session_state.clear()
                 cookie.remove('is_login')
                 cookie.remove('role')
                 cookie.remove('id_user')
+
                 time.sleep(3)
                 st.rerun()
 
 if not st.session_state.is_login:
     nav = st.navigation([st.Page(login_page, title="Login")])
 
+
 else:
     role = st.session_state.get('role', None)
+    if st.button("Logout", key='out'):
+        logout()
     if role == "admin":
         nav = st.navigation([
             st.Page("admin/dashboard.py", title="Dashboard"),
