9457a062ba4b8d0ee03e5d57ff5f04163a86bd3b
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-02-12 15:08:50 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from streamlit.delta_generator import DeltaGenerator
+import streamlit as st
+import base64
 import hashlib
 
 def metric_card(self, title, value, icon="⭐", bg_color="#ffffff"):
@@ -37,16 +39,64 @@ def metric_card(self, title, value, icon="⭐", bg_color="#ffffff"):
 
 DeltaGenerator.metric_card = metric_card
 
+@st.cache_data
+def get_images():
+    with open("images/maleprofil.png", "rb") as f:
+        male = base64.b64encode(f.read()).decode()
+        
+    with open("images/femaleprofil.png", "rb") as f:
+        female = base64.b64encode(f.read()).decode()
+    
+    with open("images/logobps.png", "rb") as f:
+        bps = base64.b64encode(f.read()).decode()
+
+    with open("images/logopendekar.png", "rb") as f:
+        logo = base64.b64encode(f.read()).decode()
+    
+    return male, female, bps, logo
+
+@st.cache_data
+def get_tahun():
+    return fetch_all("""
+            SELECT DISTINCT tahun
+            FROM periode
+            ORDER BY tahun;
+            """)
+
+def is_periode(id_periode):
+    row = fetch_one("""
+        SELECT COUNT(*) AS jumlah
+        FROM nilai_aspek
+        WHERE id_periode = %s
+        """, (id_periode,))
+    return row["jumlah"] > 0
+
+def get_bio(id_profil):
+    return fetch_one("""
+            SELECT 
+                p.id_pegawai,
+                p.nama,
+                p.jabatan,
+                p.jenis_kelamin,
+                u.email
+            FROM pegawai p
+            JOIN users u ON u.id_user = p.id_pegawai
+            WHERE p.id_pegawai = %s
+            """, (id_profil,))
+
+def fn(x):
+    return f"{x:.2f}".rstrip("0").rstrip(".")
+
 def hash(password):
     return hashlib.sha256(str.encode(password)).hexdigest()
 
 def connection():
     return mysql.connector.connect(
-        host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-        user='3z6cRQ7E5WYFy8C.root',
-        password='zSfk6X4v2VT53fQm',
-        database='SIPENDEKAR',
-        port=4000)
+        host=st.secrets['host'],
+        user=st.secrets['user'],
+        password=st.secrets['password'],
+        database=st.secrets['database'],
+        port=st.secrets['port'])
 
 def fetch_one(query, params=None):
     with connection() as conn:
@@ -60,18 +110,12 @@ def fetch_all(query, params=None):
             cursor.execute(query, params or ())
             return cursor.fetchall()
 
-def execute_one(query, params):
-    with connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(query, params or ())
-            conn.commit()
-            return cursor.rowcount
-
 def execute_all(query, params):
     with connection() as conn:
         with conn.cursor() as cursor:
             cursor.executemany(query, params)
             conn.commit()
+            
             return cursor.rowcount
 
 def sync_total(id_pegawai, id_periode):
@@ -86,7 +130,7 @@ def sync_total(id_pegawai, id_periode):
             rows = cursor.fetchall()
 
             if not rows:
-                return
+                return "gagal menambah"
 
             total = sum(r["nilai"] * r["bobot"] for r in rows)
             jml_penilai = len({r["id_penilai"] for r in rows})
@@ -99,3 +143,4 @@ def sync_total(id_pegawai, id_periode):
                 """, (id_pegawai, id_periode, hasil))
 
             conn.commit()
+            return cursor.rowcount
