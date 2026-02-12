85692769553be869898f3be53e16f10c78804533
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-02-12 16:50:19 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def metric_card(self, title, value, icon="⭐", bg_color="#ffffff"):
                 align-items:center;
                 width:100%;
             ">
-                <div style="font-size:15px; font-weight:500; margin-top:5px;">{title}</div>
+                <div style="font-size:15px; font-weight:500; margin-top:15px; margin-left:15px">{title}</div>
                 <div style="font-size:30px; font-weight:bold;">{value}</div>
             </div>
         </div>
@@ -144,3 +144,4 @@ def sync_total(id_pegawai, id_periode):
 
             conn.commit()
             return cursor.rowcount
+
