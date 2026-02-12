190a21af661054c06718bd6cf8f9756569f34b03
wiratrnn <68363766+wiratrnn@users.noreply.github.com>
2026-02-12 15:11:08 +0700

    Update profil.py

--- pages/profil.py
+++ pages/profil.py
@@ -1,38 +1,35 @@
 import streamlit as st
 import pandas as pd
-from utils import fetch_all
 import plotly.express as px
+from utils import *
 
-def fn(x):
-    return f"{x:.2f}".rstrip("0").rstrip(".")
-
-st.session_state.pop("target", None)
+biodata = get_bio(st.session_state.id_profil)
 
 @st.cache_data(ttl=300)
 def get_detail():
     return fetch_all("""
-            SELECT 
-                na.*,
-                a.nama_aspek,
-                j.judul_aspek,
-                p.bulan,
-                p.tahun,
-                nt.nilai AS total
-            FROM nilai_aspek na
-            JOIN nilai_total nt 
-                ON nt.id_pegawai = na.id_pegawai
-                AND nt.id_periode = na.id_periode
-            JOIN aspek a
-                ON a.id_aspek = na.id_aspek
-            JOIN jaspek j
-                ON j.id_jaspek = na.id_jaspek
-            JOIN periode p
-                ON p.id_periode = na.id_periode
-            WHERE na.id_pegawai = %s
-            """, (st.session_state.id_user,))
+        SELECT 
+            na.*,
+            a.nama_aspek,
+            j.judul_aspek,
+            p.bulan,
+            p.tahun,
+            nt.nilai AS total
+        FROM nilai_aspek na
+        JOIN nilai_total nt 
+            ON nt.id_pegawai = na.id_pegawai
+            AND nt.id_periode = na.id_periode
+        JOIN aspek a
+            ON a.id_aspek = na.id_aspek
+        JOIN jaspek j
+            ON j.id_jaspek = na.id_jaspek
+        JOIN periode p
+            ON p.id_periode = na.id_periode
+        WHERE na.id_pegawai = %s
+        """, (biodata['id_pegawai'],))
 
 @st.cache_data(ttl=300)
-def get_jaspek():
+def get_jaspek(tahun):
     return fetch_all("""
         SELECT 
             na.id_periode,
@@ -43,105 +40,109 @@ def get_jaspek():
             ROUND(
                 (SUM(na.nilai * a.bobot) / 100) 
                 / COUNT(DISTINCT na.id_penilai), 
-            2) AS hasil
+            2) AS total
         FROM nilai_aspek na
         JOIN aspek a ON a.id_aspek = na.id_aspek
         JOIN periode p ON p.id_periode = na.id_periode
         JOIN jaspek j ON j.id_jaspek = na.id_jaspek
-        WHERE na.id_pegawai = %s
+        WHERE na.id_pegawai = %s 
+          AND p.tahun = %s
         GROUP BY 
             na.id_periode, p.bulan, p.tahun,
             na.id_jaspek, j.judul_aspek
         ORDER BY p.tahun, p.bulan
-        """, (st.session_state.id_user,))
-
-jaspek_df = pd.DataFrame(get_jaspek())
-data_df = pd.DataFrame(get_detail())
-
-col1, col2, col3 = st.columns([1, 3, 3])
-with col1:
-    st.space()
-    if st.session_state.get("jenis_kelamin") == 1:
-        st.image("images/maleprofil.png", width=90)
-    else :
-        st.image("images/femaleprofil.png", width=90)
-
-with col2:
-    st.subheader(st.session_state.nama)
-    st.caption(f"NIP: {st.session_state.nip}")
-    st.caption(f"{st.session_state.jabatan} • {st.session_state.unit_kerja}")
-
-total_per_periode = (data_df[["id_periode", "total"]].drop_duplicates().sort_values("id_periode"))
-delta_total = total_per_periode.iloc[-1]['total'] - total_per_periode.iloc[-2]['total']
-col3.metric("Nilai Total", 
-          total_per_periode.iloc[-1]['total'], 
-          delta=delta_total, 
-          border=True)
-opsi = (
-    data_df[["id_periode", "bulan", "tahun"]]
-    .drop_duplicates()
-    .sort_values("id_periode")
-    .sort_index(ascending=True)
-)
-
-periode = st.selectbox(
-    "Pilih periode",
-    opsi,
-    format_func=lambda x: f"{opsi.set_index('id_periode').loc[x, 'bulan']}/"
-                          f"{opsi.set_index('id_periode').loc[x, 'tahun']}"
-)
+        """, (biodata['id_pegawai'], tahun))
 
 def hitung_delta(df, periode):
     now = df[df["id_periode"] == periode]
     prev = df[df["id_periode"] == periode - 1]
 
     if not now.empty and not prev.empty:
-        return now.iloc[0]["hasil"] - prev.iloc[0]["hasil"]
+        return now.iloc[0]["total"] - prev.iloc[0]["total"]
     return None
 
 def get_nilai(df, periode):
     now = df[df["id_periode"] == periode]
     if not now.empty:
-        return now.iloc[0]["hasil"]
+        return now.iloc[0]["total"]
     return 0
 
-disiplin_per_periode = jaspek_df[jaspek_df["id_jaspek"] == 1][["id_periode", "hasil"]]
-sikap_per_periode    = jaspek_df[jaspek_df["id_jaspek"] == 2][["id_periode", "hasil"]]
-kerja_per_periode    = jaspek_df[jaspek_df["id_jaspek"] == 3][["id_periode", "hasil"]]
+col1, col2, col3 = st.columns([1, 3, 3], gap='xxsmall')
+with col1:
+    st.space()
+    if biodata['jenis_kelamin'] == 1:
+        st.image("images/maleprofil.png", width=90)
+    else :
+        st.image("images/femaleprofil.png", width=90)
 
+with col2:
+    st.subheader(biodata['nama'])
+    st.caption(f"• {biodata['jabatan']} •")
+    st.caption(f"{biodata['jabatan']}")
+
+colBulan, colTahun = st.columns(2)
+Y = colTahun.selectbox("tahun periode", options=[row["tahun"] for row in get_tahun()])
+M = colBulan.selectbox("Periode Penilaian", range(1, 13),
+                        format_func=lambda x: st.session_state.bulan[x-1])
+
+periode = fetch_one("""
+            SELECT id_periode
+            FROM periode
+            WHERE tahun=%s AND bulan=%s
+            """, (Y, M))['id_periode']
+
+if not is_periode(periode):
+    st.warning(f"Belum ada data penilaian untuk periode {st.session_state.bulan[M-1]} {Y}.")
+    st.stop()
+
+
+jaspek_df = pd.DataFrame(get_jaspek(Y))
+data_df = pd.DataFrame(get_detail())
+
+total_per_periode    = data_df[["id_periode", "total"]].drop_duplicates().sort_values("id_periode")
+disiplin_per_periode = jaspek_df[jaspek_df["id_jaspek"] == 1][["id_periode", "total"]]
+sikap_per_periode    = jaspek_df[jaspek_df["id_jaspek"] == 2][["id_periode", "total"]]
+kerja_per_periode    = jaspek_df[jaspek_df["id_jaspek"] == 3][["id_periode", "total"]]
+
+delta_total    = hitung_delta(total_per_periode, periode)
 delta_disiplin = hitung_delta(disiplin_per_periode, periode)
 delta_sikap    = hitung_delta(sikap_per_periode, periode)
 delta_kerja    = hitung_delta(kerja_per_periode, periode)
 
+col3.metric(f"Nilai Total periode {st.session_state.bulan[M-1]} {Y}", 
+          get_nilai(total_per_periode, periode), 
+          delta=delta_total, 
+          border=True)
 c1, c2, c3 = st.columns(3, gap='xxsmall')
-c1.metric("Disiplin [0 - 30]",
+c1.metric(
+    "Disiplin [0 - 30]",
     get_nilai(disiplin_per_periode, periode),
     delta_disiplin,
-    chart_data=disiplin_per_periode['hasil'],
+    chart_data=disiplin_per_periode['total'],
     chart_type='line',
     border=True)
    
 c2.metric("Hasil Kerja [0 - 40]",
     get_nilai(kerja_per_periode, periode),
     delta_kerja,
-    chart_data=kerja_per_periode['hasil'],
+    chart_data=kerja_per_periode['total'],
     chart_type='line',
     border=True)
 
 c3.metric("Sikap Kerja [0 - 30]",
     get_nilai(sikap_per_periode, periode),
     delta_sikap,
-    chart_data=sikap_per_periode['hasil'],
+    chart_data=sikap_per_periode['total'],
     chart_type='line',
     border=True)
 
 jaspek_df["periode"] = jaspek_df["bulan"].astype(str) + "/" + jaspek_df["tahun"].astype(str)
-jaspek_df["hasil"] = pd.to_numeric(jaspek_df["hasil"], errors="coerce")
+jaspek_df["total"] = pd.to_numeric(jaspek_df["total"], errors="coerce")
 
 colBar, colRadar = st.columns([5,5], border=True, gap='xxsmall')
 colBar.header("Performa Pegawai per Aspek Utama")
-colBar.bar_chart(jaspek_df, x="periode", y="hasil", color="judul_aspek", 
-                horizontal=True, width=300)
+colBar.bar_chart(jaspek_df, x="periode", y="total", color="judul_aspek", 
+                horizontal=True, y_label='', width=300)
 
 urutan = ["Kepatuhan", "Ketepatan", "Inisiatif",
           "Kolaboratif","Akuntabilitas","Kuantitas",
@@ -165,7 +166,6 @@ radar_fix = (
     .melt(id_vars="nama_aspek", var_name="periode", value_name="nilai")
 )
 
-# ubah angka → bulan/tahun
 radar_fix["periode"] = radar_fix["periode"].apply(
     lambda x: f"{map_periode.loc[x, 'bulan']}/{map_periode.loc[x, 'tahun']}"
 )
@@ -189,10 +189,16 @@ fig.update_layout(
     margin=dict(t=0, b=0, l=0, r=0)
 )
 
-colRadar.header("Performa Pegawai per Sub Aspek")
+colRadar.header("Performa Pegawai per Sub-Aspek")
 colRadar.plotly_chart(fig, height=300)
 
-data_df["periode"] = (data_df["bulan"].astype(str) + "/" + data_df["tahun"].astype(str))
+
+
+
+
+
+# tabel
+data_df["periode"] = data_df["bulan"].astype(str) + "/" + data_df["tahun"].astype(str)
 
 tabel = (
     data_df[["periode"]].drop_duplicates().assign(key=1)
@@ -218,6 +224,7 @@ tabel = (
         .first()
         .rename(("Total", "Total"))
     )
+    .sort_index(axis=1)
     .reset_index()
     .fillna(0)
     .infer_objects(copy=False)
